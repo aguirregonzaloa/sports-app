ffa49b5ed646d369753a54d68f3126289bd7c684
Darkzalo <gonza1810@gmail.com>
2020-05-27 20:16:20 -0300

    Adding a Form at CreatearticleComponent

--- src/app/articles/createarticle/createarticle.component.ts
+++ src/app/articles/createarticle/createarticle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+
 
 import { IArticle } from 'src/app/models/article';
 import { ArticleService } from 'src/app/services/article/article.service';
@@ -12,10 +14,28 @@ import { ArticleService } from 'src/app/services/article/article.service';
 export class CreatearticleComponent implements OnInit {
 
   Articles$: Observable<IArticle[]>;
+  Form: FormGroup;
+  newArticle: IArticle = {name: null, description: null, price: null, category_id: null};
+
   constructor(private articleservice: ArticleService) { }
 
   ngOnInit(): void {
     this.Articles$ = this.articleservice.Article$;
+
+    this.Form = new FormGroup({
+      'name': new FormControl(null,[Validators.required,
+       Validators.pattern('[A-Za-z ].{3,30}')]),     
+       'description': new FormControl(null,[Validators.required,
+       Validators.pattern('[A-Za-z].{3,30}')]),     
+     'price': new FormControl(null, [Validators.required,
+        Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}')]),
+     'category_id': new FormControl(null, Validators.required)
+          
+   });
+  }
+
+  onSubmit(){
+    
   }
 
 }
--- src/app/models/article.ts
+++ src/app/models/article.ts
@@ -1,5 +1,5 @@
 export interface IArticle {
-id: number,
+id?: number,
 name: string,
 description:string,
 price:number,
